990ade7f81f735b7d7eaf0dcd3f4f3495542f11b
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2023-11-08 20:30:48 +0900

    Title: 달에서 무게 구하기, Time: 105ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231108/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int stone = 13;
+        double g = 0.165;
+        System.out.printf(stone + " * ");
+        System.out.printf("%.6f", g);
+        System.out.printf(" = %.6f", stone*g);
+    }
+}
