9427f5f81930bf9d569a4745b497c4a974034941
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2023-11-13 17:48:00 +0900

    Title: 가장 짧은 부분합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231113/가장 짧은 부분합/shortest-subtotal.py
+++ 231113/가장 짧은 부분합/shortest-subtotal.py
@@ -1,13 +1,20 @@
 n, s = map(int, input().split())
 numbers = list(map(int, input().split()))
 ans = n
+j = 0
+tsum = 0
+
 for i in range(n):
-    tmp_sum = 0
-    for j in range(i, n):
-        tmp_sum += numbers[j]
-        if tmp_sum == s :
-            if ans > j-i+1: ans = j-i+1
-            break
-        elif tmp_sum > s : break
+    while i<=j<n and tsum + numbers[j] >= s:
+        tsum += numbers[j] #
+        j += 1 
+        
+    if tsum + numbers[j] < s : 
+        j += 1
+        tsum+= numbers[j]
+
+    if i<=j<n:
+        ans = min(ans, j-i+1)
+        tsum -= numbers[i]
             
 print(ans)
