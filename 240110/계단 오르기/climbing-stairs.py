37af41f51b4d0c4d80cbff437b07af8638534c9d
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2024-01-10 23:42:27 +0900

    Title: 계단 오르기, Time: 5057ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240110/계단 오르기/climbing-stairs.py
+++ 240110/계단 오르기/climbing-stairs.py
@@ -1,7 +1,8 @@
 def sol(lv):
     if lv == 2 or lv == 3 : return 1
     elif lv == 1 : return 0
-    return sol(lv-2) + sol(lv-3)
+    else:
+        return sol(lv-2) + sol(lv-3)
 
 n = int(input())
 print(sol(n)%10007)
