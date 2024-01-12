e7fe9d2371535e28ed5edf0f7365ba66ab46a957
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2024-01-12 22:57:56 +0900

    Title: 알바로 부자 되기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240112/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240112/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,23 +1,25 @@
 n = int(input())
-#[start, end, pay]
+
 grid =[
     list(map(int, input().split())) for _ in range(n)
 ]
-paylist = sorted(grid, key = lambda x: x[2], reverse = True)
-endlist = sorted(grid, key = lambda x: x[1], reverse = True)
-ends = endlist[0][1]
-days = [0] * (ends +1)
 
+paylist = sorted(grid, key = lambda x: x[2], reverse = True)
+# endlist = sorted(grid, key = lambda x: x[1], reverse = True)
+# ends = endlist[0][1]
+# days = [0] * (ends +1)
+days = []
 #print(paylist)
 answer = 0
 for s, e, p in paylist:
     check = True
-    for d in range(s, e+1):
-        if days[d] == 1 : check = False
+    for ds, de in days:
+        if de >= s and ds <= e : 
+            check = False
+            break
 
     if check:
         answer += p
-        for d in range(s, e+1):
-            days[d] = 1
+        days.append((s, e))
     
 print(answer)
