e316e01aad9d39ee8b8d757e466880c141147050
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2024-01-24 18:32:36 +0900

    Title: 특별한 문자, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/특별한 문자/special-character.py
+++ 240124/특별한 문자/special-character.py
@@ -4,5 +4,11 @@ count = defaultdict(lambda : 0)
 for s in str1:
     count[s] += 1
 
-tmp = dict(zip(count.values(), count.keys()))
-print(tmp[1])
+check = False
+for k in count.keys():
+    if count[k] == 1 : 
+        check = True
+        break
+
+if check : print(k)
+else: print("None")
