e7839fcebccfadf6ec8f26eaf37522d14e6943a5
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2024-03-31 18:19:51 +0900

    Title: 데이터센터의 온도 조정, Time: 5080ms, Memory: 42MB, Status: Time Limit Exceed - CodeTree

--- 240331/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.py
+++ 240331/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.py
@@ -1,19 +1,32 @@
 n, c, g, h = map(int, input().split())
 dev = []
-count = {}
+minta = False
+maxtb = False
 for _ in range(n):
     a, b = map(int, input().split())
+    if minta == False : minta = a
+    else : 
+        minta = min(minta, a)
+        maxtb = max(maxtb, b)
+    
     dev.append([a,b])
+
+count = {}
+for a, b in dev:
     for num in range(a, b+1):
-        if num in count : count[num] += 1
-        else : count[num] = 1
+        if num in count : count[num] += g
+        else : count[num] = g
 
-temp = list(dict(sorted(count.items(), key=lambda x: x[1])).keys())[-1]
+    for num in range(minta, a):
+        if num in count : count[num] += c
+        else : count[num] = c
+    
+    for num in range(b+1, maxtb+1):
+        if num in count : count[num] += h
+        else : count[num] = h
 
-answer = 0
-for ta, tb in dev:
-    if temp < ta : answer += c
-    elif temp > tb : answer += h
-    else : answer += g
 
-print(answer)
+temp = list(dict(sorted(count.items(), key=lambda x: x[1])).values())[-1]
+
+
+print(temp)
