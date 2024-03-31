9302bfa269b4c66a96fe7c8b44fa1384009a926c
Jiwon Lee <60220571+soda723@users.noreply.github.com>
2024-03-31 21:55:32 +0900

    Title: 최대공약수 구하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240331/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,11 @@
+def maxinja(n, m):
+    answer = False
+    for i in range(n, 1, -1):
+        if n % i == 0 :
+            if m % i == 0 :
+                answer = i
+                break
+    print(answer)
+
+n, m = map(int, input().split())
+maxinja(n, m)
